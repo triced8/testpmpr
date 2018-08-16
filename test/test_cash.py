f217db78cde09476a417a39c8719e8ca860c94be
Dima <triced8@gmail.com>
2018-08-16 15:50:49 +0300

    Add Allure for test_cash and Forgot password tests

--- test/test_cash.py
+++ test/test_cash.py
@@ -1,53 +1,81 @@
 from random import randrange
 import time
 from model.credLogin import LoginCred
+import pytest
 
 random = randrange(100000000)
 
-
+@pytest.allure.step("Open cash page")
 def test_openCashPage(app):
-    app.pages.openCahsPage()
-    app.pages.frameSwitch("//*[@id='cash']/iframe")
-    assert app.warning.getOuterText("//ul[@class='filter top_menu']/li[1]") == app.text.headerCash
-    app.pages.openMainPage()
-    app.session.logout()
+    with pytest.allure.step("Open Cash page"):
+        app.pages.openCahsPage()
+    with pytest.allure.step("Switch in to the cash frame"):
+        app.pages.frameSwitch("//*[@id='cash']/iframe")
+    with pytest.allure.step("Assert that page is opened"):
+        assert app.warning.getOuterText("//ul[@class='filter top_menu']/li[1]") == app.text.headerCash
+    with pytest.allure.step("Logout from user account"):
+        app.pages.openMainPage()
+        app.session.logout()
 
-
+@pytest.allure.step("Open cash form")
 def test_openCardForm(app):
-    app.pages.openCahsPage()
-    app.pages.frameSwitch("//*[@id='cash']/iframe")
-    app.driver.find_element_by_xpath("//div[@class ='widget deposit_widget']").click()
+    with pytest.allure.step("Open Cash page"):
+        app.pages.openCahsPage()
+    with pytest.allure.step("Switch in to the cash frame"):
+        app.pages.frameSwitch("//*[@id='cash']/iframe")
+    with pytest.allure.step("Open cash form"):
+        app.driver.find_element_by_xpath("//div[@class ='widget deposit_widget']").click()
     #time.sleep(3)
-    if app.session.elementIsDisplay("//label[@for='stored_card']"):
-        app.driver.find_element_by_xpath("//label[@for='new_card']").click()
-    app.warning.waitForElementXpath("//div[@class='card_face zf-changed']")
-    assert app.session.elementIsDisplay("//div[@class='card_face zf-changed']")
-    assert app.warning.getOuterText("//label[.='Номер карты']") == "НОМЕР КАРТЫ"
-    app.pages.openMainPage()
-    app.session.logout()
+    with pytest.allure.step("If user have saved cards, open form"):
+        if app.session.elementIsDisplay("//label[@for='stored_card']"):
+            app.driver.find_element_by_xpath("//label[@for='new_card']").click()
+    with pytest.allure.step("Check fields "):
+        app.warning.waitForElementXpath("//div[@class='card_face zf-changed']")
+        assert app.session.elementIsDisplay("//div[@class='card_face zf-changed']")
+        assert app.warning.getOuterText("//label[.='Номер карты']") == "НОМЕР КАРТЫ"
+    with pytest.allure.step("Logout from user account"):
+        app.pages.openMainPage()
+        app.session.logout()
 
-
+@pytest.allure.step("Fill cash from with valid data")
 def test_fillCardForm(app):
-    app.pages.openCahsPage()
-    app.pages.frameSwitch("//*[@id='cash']/iframe")
-    app.driver.find_element_by_xpath("//div[@class ='widget deposit_widget']").click()
-    time.sleep(3)
-    if app.session.elementIsDisplay("//label[@for='stored_card']"):
-        app.driver.find_element_by_xpath("//label[@for='new_card']").click()
-        time.sleep(0.5)
-    assert app.session.elementIsDisplay("//div[@class='card_face zf-changed']")
-    assert app.warning.getOuterText("//label[.='Номер карты']") == "НОМЕР КАРТЫ"
+    with pytest.allure.step("Open Cash page"):
+        app.pages.openCahsPage()
+    with pytest.allure.step("Switch in to the cash frame"):
+        app.pages.frameSwitch("//*[@id='cash']/iframe")
+    with pytest.allure.step("Open cash form"):
+        app.driver.find_element_by_xpath("//div[@class ='widget deposit_widget']").click()
+    #time.sleep(3)
+    with pytest.allure.step("If user have saved cards, open form"):
+        if app.session.elementIsDisplay("//label[@for='stored_card']"):
+            app.driver.find_element_by_xpath("//label[@for='new_card']").click()
+    with pytest.allure.step("Check fields "):
+        app.warning.waitForElementXpath("//div[@class='card_face zf-changed']")
+        assert app.session.elementIsDisplay("//div[@class='card_face zf-changed']")
+        assert app.warning.getOuterText("//label[.='Номер карты']") == "НОМЕР КАРТЫ"
+    with pytest.allure.step("Fill in card's form"):
+        app.cash.fillCardForm()
+    with pytest.allure.step("Logout from user account"):
+        app.pages.openMainPage()
+        app.session.logout()
     app.cash.fillCardForm()
-    app.pages.openMainPage()
-    app.session.logout()
 
+
+@pytest.allure.step("Open poker client")
 def test_openGameClient(app):
-    app.pages.openMainPage()
-    app.session.login(LoginCred(username="triced", password="TestTest12"))
-    time.sleep(0.2)
-    app.driver.find_element_by_xpath("//div[@class='top_menu_buttons']//span[@class='button__inner']").click()
-    time.sleep(2)
-    app.driver.switch_to_window(app.driver.window_handles[1])
-    assert app.driver.title.startswith('PokerMatch Лобби | Ник: triced |')
-    app.driver.switch_to_window(app.driver.window_handles[0])
-    app.session.logout()
+    with pytest.allure.step("Open main page"):
+        app.pages.openMainPage()
+    with pytest.allure.step("LogIn"):
+        app.session.login(LoginCred(username="triced", password="TestTest12"))
+        time.sleep(0.2)
+    with pytest.allure.step("Open game's client"):
+        app.driver.find_element_by_xpath("//div[@class='top_menu_buttons']//span[@class='button__inner']").click()
+        time.sleep(2)
+    with pytest.allure.step("Switch at the web games's client page"):
+        app.driver.switch_to_window(app.driver.window_handles[1])
+    with pytest.allure.step("Check Nick name and title"):
+        assert app.driver.title.startswith('PokerMatch Лобби | Ник: triced |')
+    with pytest.allure.step("Switch to sate"):
+        app.driver.switch_to_window(app.driver.window_handles[0])
+    with pytest.allure.step("Logout from user account"):
+        app.session.logout()
--- test/test_forgotPassword.py
+++ test/test_forgotPassword.py
@@ -1,41 +1,117 @@
 from random import randrange
 random = randrange(100000000)
 import time
+import pytest
 
-
+@pytest.allure.step("Forgot password with valid Email")
 def test_validEmail(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail.com")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter Email in to the 'email' field"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8+3232@gmail.com")
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    time.sleep(1)
+    with pytest.allure.step("Check current URL after sending email"):
+        app.session.currentUrl("/mailed/")
+    with pytest.allure.step("Check text after sending email"):
+        assert app.warning.getOuterText("//h2[@class='main_title']") == "Мы отправили Вам ссылку для авторизации. Вы сможете сменить пароль в настройках профиля"
+
+@pytest.allure.step("Forgot password With Space before Email")
+def test_emailWithSpaceBefore(app):
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter Space before Email"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys(" triced8@gmail.com")
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
     time.sleep(0.1)
-    app.session.currentUrl("/confirmation/")
-    assert app.warning.getOuterText("//h2[@class='main_title']") == v
+    with pytest.allure.step("Check current URL after sending email"):
+        app.session.currentUrl("/confirmation/")
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText("//h2[@class='main_title']") == "Пожалуйста, проверьте Ваш e-mail ящик"
 
+@pytest.allure.step("Forgot password With space after Email")
+def test_emailWithSpaceAfter(app):
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter Space after Email"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail.com ")
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    time.sleep(0.1)
+    with pytest.allure.step("Check current URL after sending email"):
+        app.session.currentUrl("/confirmation/")
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText("//h2[@class='main_title']") == "Пожалуйста, проверьте Ваш e-mail ящик"
+
+@pytest.allure.step("Forgot password With UpperCase Email")
+def test_emailUpper(app):
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter Email with uppercase"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail.com").upper()
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    time.sleep(0.1)
+    with pytest.allure.step("Check current URL after sending email"):
+        app.session.currentUrl("/confirmation/")
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText("//h2[@class='main_title']") == "Пожалуйста, проверьте Ваш e-mail ящик"
+
+@pytest.allure.step("Forgot password with empty Email")
 def test_emptyEmail(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    assert app.warning.getOuterText("//div[@id='reminder-form']/div/span[@class='error_message']") == "E-mail не существует или не подтвержден"
-    assert app.warning.getBorderColor("//input[@id='email']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter Empty Email in to the 'email' field"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("")
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText("//div[@id='reminder-form']/div/span[@class='error_message']") == "E-mail не существует или не подтвержден"
+    with pytest.allure.step("Check warning boarder at the 'email' field"):
+        assert app.warning.getBorderColor("//input[@id='email']") == "rgba(187, 37, 37, 1)"
 
+@pytest.allure.step("Forgot password with empty captcha")
 def test_emptyCaptcha(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail.com")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    assert app.warning.getOuterText("//fieldset[@id='remind_capcha_submit']//span[@class='error_message']") == "Введено неверное значение"
-    assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter Email in to the 'email' field"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail.com")
+    with pytest.allure.step("Enter Empty captcha"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText("//fieldset[@id='remind_capcha_submit']//span[@class='error_message']") == "Введено неверное значение"
+    with pytest.allure.step("Check warning boarder at the 'captcha' field"):
+        assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
 
+@pytest.allure.step("Forgot password with wrong captcha")
 def test_wrongCaptcha(app):
     random = randrange(9999)
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail.com")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys(random)
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    assert app.warning.getOuterText("//fieldset[@id='remind_capcha_submit']//span[@class='error_message']") == "Введено неверное значение"
-    assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter Email in to the 'email' field"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail.com")
+    with pytest.allure.step("Enter wrong captcha"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys(random)
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText("//fieldset[@id='remind_capcha_submit']//span[@class='error_message']") == "Введено неверное значение"
+    with pytest.allure.step("Check warning boarder at the 'captcha' field"):
+        assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
 
 """ Not implemented yet!!!!!!!!
 
@@ -48,76 +124,85 @@ def test_emailNotexist(app):
     assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
 """
 
+@pytest.allure.step("Forgot password without AT Email")
 def test_emailWithotAt(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8gmail.com")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    assert app.warning.getOuterText("//div[@class='password_reminder_fields']/span[2]") == "E-mail не существует или не подтвержден"
-    assert app.warning.getBorderColor("//input[@id='email']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter wrong Email without AT"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8gmail.com")
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText("//div[@class='password_reminder_fields']/span[2]") == "E-mail не существует или не подтвержден"
+    with pytest.allure.step("Check warning boarder at the 'email' field"):
+        assert app.warning.getBorderColor("//input[@id='email']") == "rgba(187, 37, 37, 1)"
 
+@pytest.allure.step("Forgot password Withot Domain")
 def test_emailWithotDomain(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@.com")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    assert app.warning.getOuterText("//div[@class='password_reminder_fields']/span[2]") == "E-mail не существует или не подтвержден"
-    assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter wrong Email without Domain"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@.com")
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText("//div[@class='password_reminder_fields']/span[2]") == "E-mail не существует или не подтвержден"
+    with pytest.allure.step("Check warning boarder at the 'email' field"):
+        assert app.warning.getBorderColor("//input[@id='email']") == "rgba(187, 37, 37, 1)"
 
+@pytest.allure.step("Forgot password Withot Dot com")
 def test_emailWithotDotCom(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    assert app.warning.getOuterText("//div[@class='password_reminder_fields']/span[2]") == "E-mail не существует или не подтвержден"
-    assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter wrong Email without Dot com"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail")
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText("//div[@class='password_reminder_fields']/span[2]") == "E-mail не существует или не подтвержден"
+    with pytest.allure.step("Check warning boarder at the 'email' field"):
+        assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
 
-
+@pytest.allure.step("Forgot password Withot Email name")
 def test_emailWithotEmailName(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("@gmail.com")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter wrong Email without Email name"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("@gmail.com")
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText(
         "//div[@class='password_reminder_fields']/span[2]") == "E-mail не существует или не подтвержден"
-    assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Check warning boarder at the 'email' field"):
+        assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
 
-
+@pytest.allure.step("Forgot password Withot Dot")
 def test_emailWithotDot(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmailcom")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Open Forgot Password page"):
+        app.pages.openForgotPasswordPage()
+    with pytest.allure.step("Enter wrong Email without Dot"):
+        app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmailcom")
+    with pytest.allure.step("Enter Email in to the 'captcha' field"):
+        app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
+    with pytest.allure.step("Click on the 'Send' button"):
+        app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
+    with pytest.allure.step("Check warning message"):
+        assert app.warning.getOuterText(
         "//div[@class='password_reminder_fields']/span[2]") == "E-mail не существует или не подтвержден"
-    assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Check warning boarder at the 'email' field"):
+        assert app.warning.getBorderColor("//input[@id='captcha']") == "rgba(187, 37, 37, 1)"
 
-def test_emailWithSpaceBefore(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys(" triced8@gmail.com")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    time.sleep(0.1)
-    app.session.currentUrl("/confirmation/")
-    assert app.warning.getOuterText("//h2[@class='main_title']") == "Пожалуйста, проверьте Ваш e-mail ящик"
 
-def test_emailWithSpaceAfter(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail.com ")
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    time.sleep(0.1)
-    app.session.currentUrl("/confirmation/")
-    assert app.warning.getOuterText("//h2[@class='main_title']") == "Пожалуйста, проверьте Ваш e-mail ящик"
 
-def test_emailUpper(app):
-    app.pages.openForgotPasswordPage()
-    app.driver.find_element_by_xpath("//input[@id='email']").send_keys("triced8@gmail.com").upper()
-    app.driver.find_element_by_xpath("//input[@id='captcha']").send_keys("1111")
-    app.driver.find_element_by_xpath("//button[@id='passwordreminder']").click()
-    time.sleep(0.1)
-    app.session.currentUrl("/confirmation/")
-    assert app.warning.getOuterText("//h2[@class='main_title']") == "Пожалуйста, проверьте Ваш e-mail ящик"
 
 
 
