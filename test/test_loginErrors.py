bca0c4eb5ea851cfb0c42cc23d0c820c50b984e6
Dima <triced8@gmail.com>
2018-08-17 13:08:28 +0300

    Add Allure for loginErrors test.

--- test/test_loginErrors.py
+++ test/test_loginErrors.py
@@ -1,84 +1,134 @@
 import time
 from model.credLogin import LoginCred
 from random import randrange
-
+import pytest
 random = randrange(100000)
 
-
+@pytest.allure.step("Login with empty fields")
 def test_emptyFields(app):
-    app.session.login(LoginCred(username="", password=""))
-    app.session.captchaEntering()
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Login with empty fields"):
+        app.session.login(LoginCred(username="", password=""))
+    with pytest.allure.step("Enter captcha if it visible"):
+        app.session.captchaEntering()
+    with pytest.allure.step("Nickname warning message"):
+        assert app.warning.getOuterText(
         "(//input[@name='password'])[2]/following::div[1]") == "Почта или пароль указаны неверно"
-    assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
-    assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
-    app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
+    with pytest.allure.step("Password field's boarder color Assert"):
+        assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Login field's boarder color Assert"):
+        assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Close login pop-up by close(x) button"):
+        app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
 
-
+@pytest.allure.step("Login with empty username field")
 def test_emptyUserName(app):
-    app.session.login(LoginCred(username="", password="TestTest12"))
-    app.session.captchaEntering()
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Login with empty nickname field"):
+        app.session.login(LoginCred(username="", password="TestTest12"))
+    with pytest.allure.step("Enter captcha if it visible"):
+        app.session.captchaEntering()
+    with pytest.allure.step("Password warning message"):
+        assert app.warning.getOuterText(
         "(//input[@name='password'])[2]/following::div[1]") == "Почта или пароль указаны неверно"
-    assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
-    assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
-    app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
+    with pytest.allure.step("Password field's boarder color Assert"):
+        assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Login field's boarder color Assert"):
+        assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Close login pop-up by close(x) button"):
+        app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
 
 
+@pytest.allure.step("Login with empty password field")
 def test_emptyPassword(app):
-    app.session.login(LoginCred(username="triced", password=""))
-    app.session.captchaEntering()
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Login with empty password field"):
+        app.session.login(LoginCred(username="triced", password=""))
+    with pytest.allure.step("Enter captcha if it visible"):
+        app.session.captchaEntering()
+    with pytest.allure.step("Password warning message"):
+        assert app.warning.getOuterText(
         "(//input[@name='password'])[2]/following::div[1]") == "Почта или пароль указаны неверно"
-    assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
-    assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
-    app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
+    with pytest.allure.step("Password field's boarder color Assert"):
+        assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Login field's boarder color Assert"):
+        assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Close login pop-up by close(x) button"):
+        app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
 
-
+@pytest.allure.step("Login with not exist username")
 def test_loginNotExistUser(app):
-    app.session.login(LoginCred(username="triced" + str(random), password="TestTest12"))
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Login with not exist user by nickname"):
+        app.session.login(LoginCred(username="triced" + str(random), password="TestTest12"))
+    with pytest.allure.step("Enter captcha if it visible"):
+        app.session.captchaEntering()
+    with pytest.allure.step("Password warning message"):
+        assert app.warning.getOuterText(
         "(//input[@name='password'])[2]/following::div[1]") == "Почта или пароль указаны неверно"
-    assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
-    assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
-    app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
+    with pytest.allure.step("Password field's boarder color Assert"):
+        assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Login field's boarder color Assert"):
+        assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Close login pop-up by close(x) button"):
+        app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
 
-
+@pytest.allure.step("Login with dot after nickname")
 def test_nickNameWithDoteAfter(app):
-    app.session.login(LoginCred(username="triced.", password="TestTest12"))
-    app.session.captchaEntering()
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Login with dot after nickname"):
+        app.session.login(LoginCred(username="triced.", password="TestTest12"))
+    with pytest.allure.step("Enter captcha if it visible"):
+        app.session.captchaEntering()
+    with pytest.allure.step("Password warning message"):
+        assert app.warning.getOuterText(
         "(//input[@name='password'])[2]/following::div[1]") == "Почта или пароль указаны неверно"
-    assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
-    assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
-    app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
+    with pytest.allure.step("Password field's boarder color Assert"):
+        assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Login field's boarder color Assert"):
+        assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Close login pop-up by close(x) button"):
+        app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
 
-
+@pytest.allure.step("Login with dot after email")
 def test_emailWithDoteAfter(app):
-    app.session.login(LoginCred(username="triced8+3030@gmail.com.", password="TestTest12"))
-    app.session.captchaEntering()
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Login with dot after email"):
+        app.session.login(LoginCred(username="triced8+3030@gmail.com.", password="TestTest12"))
+    with pytest.allure.step("Enter captcha if it visible"):
+        app.session.captchaEntering()
+    with pytest.allure.step("Password warning message"):
+        assert app.warning.getOuterText(
         "(//input[@name='password'])[2]/following::div[1]") == "Почта или пароль указаны неверно"
-    assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
-    assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
-    app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
+    with pytest.allure.step("Password field's boarder color Assert"):
+        assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Login field's boarder color Assert"):
+        assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Close login pop-up by close(x) button"):
+        app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
 
-
+@pytest.allure.step("Login with dot before nickname")
 def test_nickNameWithDoteBefore(app):
-    app.session.login(LoginCred(username=".triced", password="TestTest12"))
-    app.session.captchaEntering()
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Login with dot before nickname"):
+        app.session.login(LoginCred(username=".triced", password="TestTest12"))
+    with pytest.allure.step("Enter captcha if it visible"):
+        app.session.captchaEntering()
+    with pytest.allure.step("Password warning message"):
+        assert app.warning.getOuterText(
         "(//input[@name='password'])[2]/following::div[1]") == "Почта или пароль указаны неверно"
-    assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
-    assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
-    app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
+    with pytest.allure.step("Password field's boarder color Assert"):
+        assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Login field's boarder color Assert"):
+        assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Close login pop-up by close(x) button"):
+        app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
 
-
+@pytest.allure.step("Login with caps password")
 def test_loginPasswordCaps(app):
-    app.session.login(LoginCred(username="triced8+3030@gmail.com", password="TestTest12".upper()))
-    app.session.captchaEntering()
-    assert app.warning.getOuterText(
+    with pytest.allure.step("Login with caps password"):
+        app.session.login(LoginCred(username=".triced", password="TestTest12".upper()))
+    with pytest.allure.step("Enter captcha if it visible"):
+        app.session.captchaEntering()
+    with pytest.allure.step("Password warning message"):
+        assert app.warning.getOuterText(
         "(//input[@name='password'])[2]/following::div[1]") == "Почта или пароль указаны неверно"
-    assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
-    assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
-    app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
+    with pytest.allure.step("Password field's boarder color Assert"):
+        assert app.warning.getBorderColor("//div[@id='login']//input[@name='password'][2]") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Login field's boarder color Assert"):
+        assert app.warning.getBorderColor("//input[@name='login']") == "rgba(187, 37, 37, 1)"
+    with pytest.allure.step("Close login pop-up by close(x) button"):
+        app.driver.find_element_by_xpath("//div[@id='login']//button[@aria-label='Close']").click()
